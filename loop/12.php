d37b956be5413a47f95b379e5aecc27d51abb57a
freedom9 <nfreedom9@gmail.com>
2017-07-12 20:14:19 +0900

    test

---
+++ loop/12.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+for($i = 0; $i <= 5; $i++){
+	for($j = 0; $j <= 5; $j++){
+		echo "coding everybody {$i} {$j}<br />";
+	}
+}
+
+?>
+</body>
+</html>
---
+++ loop/7.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+echo 'coding everybody 1<br />';
+echo 'coding everybody 2<br />';
+echo 'coding everybody 3<br />';
+echo 'coding everybody 4<br />';
+echo 'coding everybody 5<br />';
+echo 'coding everybody 6<br />';
+echo 'coding everybody 7<br />';
+echo 'coding everybody 8<br />';
+echo 'coding everybody 9<br />';
+echo 'coding everybody 10<br />';
+?>
+</body>
+</html>
