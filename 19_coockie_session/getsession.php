b0ac7f1b9ce2d26b21f6831aa38eccbebe51529d
freedom9 <nfreedom9@gmail.com>
2017-07-24 15:34:17 +0900

    test

--- 19_coockie_session/getsession.php
+++ 19_coockie_session/getsession.php
@@ -1,11 +1,11 @@
 <?php
 ini_set("display_errors", "1");
-session_save_path(getcwd().'/session');
+session_save_path('./session');
 session_start();
 echo $_SESSION['title'];
 
 echo "<br />";
 echo "<br />";
 
-echo file_get_contents(getcwd().'/session/sess_' . session_id());
+echo file_get_contents('./session/sess_' . session_id());
 ?>
--- 19_coockie_session/setsession.php
+++ 19_coockie_session/setsession.php
@@ -1,5 +1,5 @@
 <?php
-session_save_path(getcwd().'/session');
+session_save_path('./session');
 session_start();
 $_SESSION['title'] = '생활코딩';
 header('Location: ./getsession.php');
